ccde3f09ab5aa36357ab08aa7b4ab87f167ba5aa
LudOhm <ludivine.perrier.babin@gmail.com>
2026-02-05 11:41:13 +0100

    add java

--- src/main/java/com/example/demo_nexus/DemoNexusApplication.java
+++ src/main/java/com/example/demo_nexus/DemoNexusApplication.java
@@ -2,6 +2,8 @@ package com.example.demo_nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoNexusApplication {
@@ -10,4 +12,12 @@ public class DemoNexusApplication {
 		SpringApplication.run(DemoNexusApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloWorldController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World!";
+	}
 }
