365bf6cac04381a9809d844aa4b833b65cf62110
LudOhm <ludivine.perrier.babin@gmail.com>
2026-02-05 10:30:58 +0100

    first commit

---
+++ src/main/java/com/example/demo_nexus/DemoNexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_nexus/DemoNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
